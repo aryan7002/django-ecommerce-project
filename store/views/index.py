017e32a3fd11c0f555712debf334be44ea3e01fc
aryan <aryandangar7@gmail.com>
2021-04-27 20:10:52 +0530

    work 13

--- store/views/index.py
+++ store/views/index.py
@@ -25,11 +25,17 @@ class Index(View):
             cart = {}
             cart[product] = 1
         request.session['cart'] = cart
+        print(request.session['cart'])
+        
         return redirect('homepage')
 
     def get(self, request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session.cart = {}
+
         products = None
-        
+        #request.session.get('cart').clear()
         categories = Category.get_all_categories()
         processors = Processor.get_all_processors()
         gpus = GPU.get_all_gpus()
