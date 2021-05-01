7ca5c68601040908db6ada80918f446168605d33
aryan <aryandangar7@gmail.com>
2021-05-01 11:20:57 +0530

    work 16

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -8,3 +8,7 @@ def is_in_cart(product , cart):
         if int(id) == product.id:
             return True
     return False
+
+@register.filter(name='cart_quantity')
+def cart_quantity(product , cart):
+    return cart[str(product.id)]
