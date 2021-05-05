1245bbdf6408cd9b8c8270c78e20029032fa7a57
aryan <aryandangar7@gmail.com>
2021-05-05 18:41:07 +0530

    work 20

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from .views import  login, signup
 from .views.index import Index
+from .views.login import logout,Login
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', signup.signup, name='signup'),
     path('login', login.Login.as_view(), name='login'),
+    path('logout', logout, name='logout'),
 ]
--- store/views/login.py
+++ store/views/login.py
@@ -31,3 +31,6 @@ class Login(View):
         print(email,password)
         return render(request,'login.html',{'error':error_message})
 
+def logout(request):
+    request.session.clear()
+    return redirect('login')
